c5b374ff0e2d68a6d073fed787e9a4fbbefde15b
Anto <antony.arslanyan@ensta-bretagneorg>
2019-01-09 11:01:13 +0100

    suppression build et devel ?

--- workspaceRos/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shraay/Documents/Robot_dufur/workspaceRos/src
-# Build directory: /home/shraay/Documents/Robot_dufur/workspaceRos/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
