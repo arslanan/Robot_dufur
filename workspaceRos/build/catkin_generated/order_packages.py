2517d565b80b9d6679695148731da642659e9d76
shraay <sarahdelmas17@hotmail.com>
2019-01-09 10:39:22 +0100

    workspaceROS

---
+++ workspaceRos/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/shraay/Documents/Robot_dufur/workspaceRos/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/shraay/Documents/gazebo_t/devel;/opt/ros/kinetic".split(';') if "/home/shraay/Documents/gazebo_t/devel;/opt/ros/kinetic" != "" else []
